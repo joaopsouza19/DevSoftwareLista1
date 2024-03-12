d9b501dfdf7a17bbd99fd4e1f90bdbfd722aa7e9
João <jpjoaopedro201444@gmail.com>
2024-03-12 00:15:20 -0300

    Exercicio 5

---
+++ Lista 1/Exercicio05.java
@@ -0,0 +1,15 @@
+public class Exercicio05 {
+
+    public static void executar(){
+        double A = Prompt.lerInteiro("Digite o número A: ");
+        double B = Prompt.lerInteiro("Digite o número B: ");
+        double valorA = B;
+        double valorB = A;
+        Prompt.imprimir("Valor original de A: " + A);
+        Prompt.imprimir("Valor original de B: " + B);
+        Prompt.imprimir("Valor trocado de A: " + valorA);
+        Prompt.imprimir("Valor trocado de B: " + valorB);
+
+    }
+    
+}
