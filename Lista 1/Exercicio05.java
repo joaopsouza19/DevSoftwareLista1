aae6750be3802434569e0e61f211b114971a7238
João <jpjoaopedro201444@gmail.com>
2024-03-12 00:27:10 -0300

    Exercicio 5

--- Lista 1/Exercicio05.java
+++ Lista 1/Exercicio05.java
@@ -1,8 +1,8 @@
 public class Exercicio05 {
 
     public static void executar(){
-        double A = Prompt.lerInteiro("Digite o número A: ");
-        double B = Prompt.lerInteiro("Digite o número B: ");
+        double A = Prompt.lerDecimal("Digite o número A: ");
+        double B = Prompt.lerDecimal("Digite o número B: ");
         double valorA = B;
         double valorB = A;
         Prompt.imprimir("Valor original de A: " + A);
